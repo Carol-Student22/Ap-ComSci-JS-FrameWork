ba527707501028b08d5d3b38f575274579b635be
Carol-Student22 <carol.lian22@gmail.com>
2021-10-14 12:29:02 -0400

    .

---
+++ project/src/components/Start.vue
@@ -0,0 +1,23 @@
+<template>
+    <section id="main" class="to-be-changed-later">
+        <h1 class="title">Random Title - Temporary Placement </h1>
+        <h1>{{ msg }}</h1>
+
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Start',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+#main {
+    background-color: red ;
+}
+
+</style>
